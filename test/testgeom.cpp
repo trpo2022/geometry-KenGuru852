569f6cd9daebee5723c1a4f4de06b3c2195938ec
KenGuru852 <pritula.alex91@mail.ru>
2022-05-12 19:06:09 +0700

    Fixed tests

--- test/testgeom.cpp
+++ test/testgeom.cpp
@@ -18,23 +18,23 @@
 #include <string>
 #include <vector>
 
-CTEST(Perimetr_Tri, test_true)
+CTEST(Perimetr_Tri, test_true1)
 {
     CTEST_LOG("Checking Perimetr_Tri() true function");
-    const int expected = 10;
+    const float expected = 10;
     float a = 2.5;
     float b = 3.5;
     float c = 4;
     float result = Perimetr_Tri(a, b, c);
     ASSERT_DBL_NEAR(expected, result);
 }
-CTEST(Perimetr_Tri, test_false)
+CTEST(Perimetr_Tri, test_true2)
 {
-    CTEST_LOG("Checking Perimetr_Tri() false function");
-    const int expected = 11;
+    CTEST_LOG("Checking Perimetr_Tri() true function");
+    const float expected = 12.5;
     float a = 2.5;
     float b = 3.5;
-    float c = 4;
+    float c = 6.5;
     float result = Perimetr_Tri(a, b, c);
     ASSERT_DBL_NEAR(expected, result);
 }
@@ -48,7 +48,7 @@ CTEST(check_function, circle_false)
 {
     CTEST_LOG("Checking check_function() on circle false");
     bool real = check_function("ciwpoghrcle");
-    ASSERT_TRUE(real);
+    ASSERT_FALSE(real);
 }
 CTEST(check_function, triangle_true)
 {
@@ -60,9 +60,9 @@ CTEST(check_function, triangle_false)
 {
     CTEST_LOG("Checking check_function() on triangle false");
     bool real = check_function("triangleajkghesjkhdskjgh");
-    ASSERT_TRUE(real);
+    ASSERT_FALSE(real);
 }
-CTEST(Dlina_Storony, dlina_true)
+CTEST(Dlina_Storony, dlina_true1)
 {
     CTEST_LOG("Checking Dlina_Storony() on true");
     float x1 = 2.5;
@@ -73,18 +73,18 @@ CTEST(Dlina_Storony, dlina_true)
     float result = Dlina_Storony(x1, y1, x2, y2);
     ASSERT_DBL_NEAR(expected, result);
 }
-CTEST(Dlina_Storony, dlina_false)
+CTEST(Dlina_Storony, dlina_true2)
 {
-    CTEST_LOG("Checking Dlina_Storony() on false");
-    float x1 = 2.5;
-    float y1 = 3.5;
-    float x2 = 6.5;
-    float y2 = 0.5;
-    float expected = 6;
+    CTEST_LOG("Checking Dlina_Storony() on true");
+    float x1 = 5;
+    float y1 = 7;
+    float x2 = 13;
+    float y2 = 1;
+    float expected = 10;
     float result = Dlina_Storony(x1, y1, x2, y2);
     ASSERT_DBL_NEAR(expected, result);
 }
-CTEST(Circle, check_area_true)
+CTEST(Circle, check_area_true1)
 {
     CTEST_LOG("Checking Circle() on area true");
     circle test;
@@ -95,18 +95,18 @@ CTEST(Circle, check_area_true)
     float expected = 7.06858;
     ASSERT_DBL_NEAR(expected, result);
 }
-CTEST(Circle, check_area_false)
+CTEST(Circle, check_area_true2)
 {
-    CTEST_LOG("Checking Circle() on area false");
+    CTEST_LOG("Checking Circle() on area true");
     circle test;
-    std::string testing = "circle(0 0, 1.5)";
+    std::string testing = "circle(7 9, 4.13)";
     int j = 7;
     test = Circle(testing, j);
     float result = test.area;
-    float expected = 11.1111;
+    float expected = 53.5858;
     ASSERT_DBL_NEAR(expected, result);
 }
-CTEST(Circle, check_per_true)
+CTEST(Circle, check_per_true1)
 {
     CTEST_LOG("Checking Circle() on perimetr true");
     circle test;
@@ -117,18 +117,18 @@ CTEST(Circle, check_per_true)
     float expected = 9.42478;
     ASSERT_DBL_NEAR(expected, result);
 }
-CTEST(Circle, check_per_false)
+CTEST(Circle, check_per_true2)
 {
-    CTEST_LOG("Checking Circle() on perimetr false");
+    CTEST_LOG("Checking Circle() on perimetr true");
     circle test;
-    std::string testing = "circle(0 0, 1.5)";
+    std::string testing = "circle(7 9, 4.13)";
     int j = 7;
     test = Circle(testing, j);
     float result = test.per;
-    float expected = 1.111;
+    float expected = 25.9496;
     ASSERT_DBL_NEAR(expected, result);
 }
-CTEST(Circle, check_rad_true)
+CTEST(Circle, check_rad_true1)
 {
     CTEST_LOG("Checking Circle() on radius true");
     circle test;
@@ -139,18 +139,18 @@ CTEST(Circle, check_rad_true)
     float expected = 1.5;
     ASSERT_DBL_NEAR(expected, result);
 }
-CTEST(Circle, check_rad_false)
+CTEST(Circle, check_rad_true2)
 {
-    CTEST_LOG("Checking Circle() on radius false");
+    CTEST_LOG("Checking Circle() on radius true");
     circle test;
-    std::string testing = "circle(0 0, 1.5)";
+    std::string testing = "circle(7 9, 4.13)";
     int j = 7;
     test = Circle(testing, j);
     float result = test.rad;
-    float expected = 2;
+    float expected = 4.13;
     ASSERT_DBL_NEAR(expected, result);
 }
-CTEST(Circle, check_string_true)
+CTEST(Circle, check_string_true1)
 {
     CTEST_LOG("Checking Circle() on string true");
     circle test;
@@ -161,18 +161,18 @@ CTEST(Circle, check_string_true)
     std::string result = test.func_name;
     ASSERT_STR(expected.c_str(), result.c_str());
 }
-CTEST(Circle, check_string_false)
+CTEST(Circle, check_string_true2)
 {
-    CTEST_LOG("Checking Circle() on string false");
+    CTEST_LOG("Checking Circle() on string true");
     circle test;
-    std::string testing = "circle(0 0, 1.5)";
+    std::string testing = "circle(7 9, 4.13)";
     int j = 7;
     test = Circle(testing, j);
-    std::string expected = "circle(00,1.5)";
+    std::string expected = "circle(7 9, 4.13)";
     std::string result = test.func_name;
     ASSERT_STR(expected.c_str(), result.c_str());
 }
-CTEST(circle_point, check_point_true)
+CTEST(circle_point, check_point_true1)
 {
     CTEST_LOG("Checking on point true");
     circle test;
@@ -184,19 +184,19 @@ CTEST(circle_point, check_point_true)
     ASSERT_DBL_NEAR(expected.x, result.x);
     ASSERT_DBL_NEAR(expected.y, result.y);
 }
-CTEST(circle_point, check_point_false)
+CTEST(circle_point, check_point_true2)
 {
-    CTEST_LOG("Checking on point false");
+    CTEST_LOG("Checking on point true");
     circle test;
-    std::string testing = "circle(0 0, 1.5)";
+    std::string testing = "circle(7 9, 4.13)";
     int j = 7;
     test = Circle(testing, j);
     Point result = test.center;
-    Point expected(2, 0);
+    Point expected(7, 9);
     ASSERT_DBL_NEAR(expected.x, result.x);
     ASSERT_DBL_NEAR(expected.y, result.y);
 }
-CTEST(findnumber, check_true)
+CTEST(findnumber, check_true1)
 {
     CTEST_LOG("Checking on findnumber_true");
     std::string testing = "circle(8 0, 1.5)";
@@ -205,12 +205,12 @@ CTEST(findnumber, check_true)
     std::string result = findnumber(testing, j);
     ASSERT_STR(expected.c_str(), result.c_str());
 }
-CTEST(findnumber, check_false)
+CTEST(findnumber, check_true2)
 {
-    CTEST_LOG("Checking on findnumber_false");
-    std::string testing = "circle(8 0, 1.5)";
+    CTEST_LOG("Checking on findnumber_true");
+    std::string testing = "circle(7 9, 1.5)";
     int j = 7;
-    std::string expected = "0";
+    std::string expected = "7";
     std::string result = findnumber(testing, j);
     ASSERT_STR(expected.c_str(), result.c_str());
 }
@@ -246,7 +246,7 @@ CTEST(intersections, check_false)
     j = 7;
     arr.push_back(Circle(circle3, j));
     bool real = findIntersections(arr);
-    ASSERT_TRUE(real);
+    ASSERT_FALSE(real);
 }
 CTEST(isdigit, check_true)
 {
@@ -262,9 +262,9 @@ CTEST(isdigit, check_false)
     std::string testing = "asdf";
     int j = 0;
     bool result = is_number(testing, j);
-    ASSERT_TRUE(result);
+    ASSERT_FALSE(result);
 }
-CTEST(Triangle, check_str_true)
+CTEST(Triangle, check_str_true1)
 {
     CTEST_LOG("Checking Triangle() on str_true");
     triangle test;
@@ -275,18 +275,18 @@ CTEST(Triangle, check_str_true)
     std::string result = test.func_name;
     ASSERT_STR(expected.c_str(), result.c_str());
 }
-CTEST(Triangle, check_str_false)
+CTEST(Triangle, check_str_true2)
 {
     CTEST_LOG("Checking Triangle() on str_false");
     triangle test;
-    std::string testing = "triangle((3 -2, 3 2, 1 0, 3 -2))";
+    std::string testing = "triangle((-5.0 -2, -1 0.0, -3.0 2.0, -3 -2))";
     int j = 10;
     test = Triangle(testing, j);
-    std::string expected = "triangle((31235 -2, 3 2, 1 0, 3 -2))";
+    std::string expected = "triangle((-5.0 -2, -1 0.0, -3.0 2.0, -3 -2))";
     std::string result = test.func_name;
     ASSERT_STR(expected.c_str(), result.c_str());
 }
-CTEST(Triangle, check_area_true)
+CTEST(Triangle, check_area_true1)
 {
     CTEST_LOG("Checking Triangle() on area true");
     triangle test;
@@ -297,11 +297,11 @@ CTEST(Triangle, check_area_true)
     float expected = 4;
     ASSERT_DBL_NEAR(expected, result);
 }
-CTEST(Triangle, check_area_false)
+CTEST(Triangle, check_area_true2)
 {
     CTEST_LOG("Checking Triangle() on area false");
     triangle test;
-    std::string testing = "triangle((3 -2, 3 2, 1 0, 3 -2))";
+    std::string testing = "triangle((-5.0 -2, -1 0.0, -3.0 2.0, -3 -2))";
     int j = 10;
     test = Triangle(testing, j);
     float result = test.area;
